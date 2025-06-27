3bb6e63d6c740a9e5fed934c7df4f87570036a49
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2025-06-27 16:38:16 +0530

    link updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Get Vite mode (e.g., 'development' or 'production')
+const mode = process.env.NODE_ENV;
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    base: 'my-portfolio',
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/my-portfolio/", // 👈 must match repo name with slashes
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
